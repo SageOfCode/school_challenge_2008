16676f2daa1d2737977469d57dadb16a04f4c680
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-19 13:51:22 -0600

    initial commit

---
+++ lib/school.rb
@@ -0,0 +1,17 @@
+class School
+
+  attr_reader :start_time, :hours_in_school_day, :student_names
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  # def student_names
+  #   @student_names = []
+  # end
+
+  def add_student_name(student_names)
+    @student_names << student_names
+  end
+end
